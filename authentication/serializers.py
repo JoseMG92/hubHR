4983316c15a6e4bf4139fd0af3e18fbe73123c51
JoseMG92 <ppmezag@gmail.com>
2022-05-19 13:11:31 -0500

    Muestra job_title

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -11,4 +11,4 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
          model = Users
-         fields = ['id', 'first_name', 'last_name', 'username']
+         fields = ['id', 'username', 'first_name', 'last_name', 'job_title']
